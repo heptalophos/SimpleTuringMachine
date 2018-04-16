6aaecde6ba73b9a3caeec78279fa73630228525b
Chris NP <zerodeltahedge@gmail.com>
2018-04-16 16:41:22 +0300

    1st commit

--- test/turingmachine-test.py
+++ test/turingmachine-test.py
@@ -22,6 +22,8 @@ class TuringMachineTestCase(unittest.TestCase):
         ensure(self.tur).is_a(TuringMachine)
         ensure(self.tur.tape).is_a(list).of(str)
         ensure(self.tur.state_table).is_a(dict).of(tuple).to(tuple)
+        # see Turing machine executing 5 iterations (there is no halt state)
+        print('\n'.join(self.tur.simulate(self.tur.state_table, 5)))
 
     def test_last_state_5_iterations(self):
         ensure(self.tur.simulate(self.tur.state_table, 5)[-1]).equals("  s2: XBBB\n       ^")
