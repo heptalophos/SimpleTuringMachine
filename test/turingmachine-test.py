32d4cdc5d34aa72c52e898569f37b8b122b79a8d
Chris NP <zerodeltahedge@gmail.com>
2018-04-16 16:15:07 +0300

    1st commit

--- test/turingmachine-test.py
+++ test/turingmachine-test.py
@@ -16,13 +16,44 @@ class TuringMachineTestCase(unittest.TestCase):
     
     def setUp(self):
         self.tur = TuringMachine()
-       
 
     def test_init(self):
         print("\n")
         ensure(self.tur).is_a(TuringMachine)
-        ensure(self.tur.tape1).is_a_list_of(str)
-        # ensure(self.tur.state_table1).is_a_dict_of((chr, chr)).to((chr, chr, chr))
+        ensure(self.tur.tape).is_a(list).of(str)
+        ensure(self.tur.state_table).is_a(dict).of(tuple).to(tuple)
+
+    def test_last_state_5_iterations(self):
+        ensure(self.tur.simulate(self.tur.state_table, 5)[-1]).equals("  s2: XBBB\n       ^")
+
+    def test_last_state_4_iterations(self):
+        ensure(self.tur.simulate(self.tur.state_table, 4)[-1]).equals("  s1: BBBB\n      ^")
+
+    def test_last_state_3_iterations(self):   
+        ensure(self.tur.simulate(self.tur.state_table, 3)[-1]).equals("  s4: BBBB\n       ^")
+
+    def test_last_state_2_iterations(self):
+        ensure(self.tur.simulate(self.tur.state_table, 2)[-1]).equals("  s3: XBBB\n      ^")
+    
+    def test_last_state_1_iteration(self):
+        ensure(self.tur.simulate(self.tur.state_table, 1)[-1]).equals("  s2: XBBB\n       ^")
+
+    def test_output(self):
+        ensure(self.tur.simulate(self.tur.state_table, 3)).is_a(list).of(str)
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 1)[0]).equals('  s1: BBBB\n      ^')
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 2)[0]).equals('  s1: BBBB\n      ^')
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 3)[0]).equals('  s1: BBBB\n      ^')
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 4)[0]).equals('  s1: BBBB\n      ^')
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 2)[1]).equals('  s2: XBBB\n       ^')
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 3)[2]).equals('  s3: XBBB\n      ^')
+        self.setUp()
+        ensure(self.tur.simulate(self.tur.state_table, 4)[3]).equals('  s4: BBBB\n       ^')
 
     def tearDown(self):
         del self.tur
